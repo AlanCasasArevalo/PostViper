5635295a371a9541b67b721d55511a327ced27c0
Alan Casas <alancasasarevalo@gmail.com>
2021-05-07 23:23:51 +0200

    * Some tableview utils were added

---
+++ PostVIPER/Commons/ReusableView.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+
+protocol ReusableView: class {
+    static var defaultReuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    
+    static var defaultReuseIdentifier: String {
+        return String(describing: self)
+    }
+}
